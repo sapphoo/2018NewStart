656b3568469725357b32eec01bbe0d5fbc89eb87
sapphoo <wudi.echo@gmail.com>
2018-03-19 07:57:45 +0800

    update 2018-03-18

--- mdn/learning-area/javascript/intro-to-js-1/gallery-start/main.js
+++ mdn/learning-area/javascript/intro-to-js-1/gallery-start/main.js
@@ -5,10 +5,29 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+var i;
+for (i = 1; i <= 5; i++) {
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', "images/pic" + i + ".jpg");
+    thumbBar.appendChild(newImage);
+}
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+document.getElementsByClassName("thumb-bar")[0].addEventListener("click", function(e) {
+    if (e.target) {
+        displayedImage.setAttribute("src", e.target.getAttribute("src"));
+    }
+});
+
 
 /* Wiring up the Darken/Lighten button */
-
+btn.addEventListener("click", function(e) {
+    if (e.target) {
+        if (overlay.style.backgroundColor === "rgba(1, 1, 1, 0.2)") {
+            overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+            btn.textContent = "Darken";
+        } else {
+            overlay.style.backgroundColor = "rgba(1, 1, 1, 0.2)";
+            btn.textContent = "Light";
+        }
+    }
+});
