7bf02ab304c4d60633225a9b029d3372eb97cae3
w00422349 <wudi41@huawei.com>
2018-03-20 17:39:17 +0800

    update 3_20

---
+++ mdn/learning-area/javascript/oojs/bouncing_balls/index.js
@@ -0,0 +1,141 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    var num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+//构造函数
+function Shape(x, y, velX, velY, exists) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.exists = exists;
+}
+
+function Ball(x, y, velX, velY, color, size, exists) {
+    Shape.call(this, x, y, velX, velY, exists);
+    this.color = color;
+    this.size = size;
+}
+
+//绘制
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+//更新数据
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+function EvilCircle(x, y, exists) {
+    Shape.call(this, x, y, exists);
+    this.color = "white";
+    this.size = 10;
+    this.velX = 20;
+    this.vely = 20;
+}
+
+EvilCircle.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function () {
+    if ((this.x + this.size) >= width) {
+        this.x -= this.x + this.size - width;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.x += this.size - this.x;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.y -= this.y + this.size - height;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.y += this.size - this.y;
+    }
+}
+
+
+
+//撞击检测
+Ball.prototype.collisionDetect = function () {
+    for (var j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+}
+
+//动起来
+var balls = [];
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    while (balls.length < 25) {
+        var ball = new Ball(
+            random(0, width),
+            random(0, height),
+            random(-7, 7),
+            random(-7, 7),
+            'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+            random(10, 20),
+            true
+        );
+        balls.push(ball);
+    }
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    //requestAnimationFrame(loop);
+}
+
+loop();
