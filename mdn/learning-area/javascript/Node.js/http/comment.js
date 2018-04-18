6f5a9434451944cb7c90bf9c2211acc94f6c1f67
sapphoo <wudi.echo@gmail.com>
2018-04-18 23:44:14 +0800

    update 2018-04-18 nodejs auto comment

---
+++ mdn/learning-area/javascript/Node.js/http/comment.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var querystring = require('querystring');
+
+var postData = querystring.stringify({
+    'content': '懒得找其他网站了，就跟这儿试一下吧',
+    'mid': 8837
+});
+
+var options = {
+    hostname: 'www.imooc.com',
+    port: 80,
+    path: '/course/docomment',
+    method: 'POST',
+    headers: {
+        'Accept': 'application / json,text / javascript,*/*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Content-Length': postData.length,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'imooc_uuid=a4045fd7-dc12-4537-94bc-42e8d446d965; imooc_isnew_ct=1523762749; loginstate=1; apsid=VhY2E5M2Q4YTQxNjI4MzJlNjJiMzYwZTUyNzVmNTYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMjQ0NTM4MQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAyNzgzMjYzMjRAcXEuY29tAAAAAAAAAAAAAAAAAAAAAGM2Zjc2MzIzMjRjMjQzNWJjZjA0YjFiMjgwMjk3ZGY2TMbSWkzG0lo%3DOT; last_login_username=278326324%40qq.com; imooc_isnew=2; mc_channel=bdqdrmjt; mc_marking=6591df326769bdbc2fb5497812295288; UM_distinctid=162ce812beb16c-0deecf5ce1a245-b34356b-151800-162ce812beccd; CNZZDATA1261110065=756543833-1523880982-https%253A%252F%252Fwww.baidu.com%252F%7C1523880982; PHPSESSID=ng583uaoves0fno2s4k21h42c0; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1523762755,1523882995,1523883172,1523883512; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1524052493; cvde=5ad49df6d8a86-15',
+        'Host': 'www.imooc.com',
+        'Origin': 'https://www.imooc.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://www.imooc.com/video/8837',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+}
+
+//var req = http.request(options);
+//回调函数可选
+var req = http.request(options, function(res) {
+    console.log(res.statusCode);
+    console.log(JSON.stringify(res.headers));
+
+    res.on('data', function(chunk) {
+        console.log(Buffer.isBuffer(chunk));
+    })
+
+    res.on('end', function() {
+        console.log('comment is done');
+    })
+
+})
+req.on('error', function(e) {
+    console.log(e.message);
+})
+req.write(postData);
+
+req.end();
