3a4c51d945b392b9157642a08a6877002c615b83
sapphoo <wudi.echo@gmail.com>
2018-05-10 00:12:53 +0800

    update

--- demos/carousels/javaScript/main.js
+++ demos/carousels/javaScript/main.js
@@ -1,6 +1,6 @@
-window.onload = function(){
+window.onload = function() {
     var carousels = document.getElementById("carousels_div");
-    
+
     var opt_btns = document.getElementsByClassName("operate_button");
     var left_btn = document.getElementById('left_button');
     var right_btn = document.getElementById('right_button');
@@ -15,48 +15,67 @@ window.onload = function(){
 
     var timer = setInterval(loop, 2000);
 
-    function loop(){
+    function loop() {
         current_index++;
         changeCarousel();
     }
 
-    function changeCarousel(){
-        if(current_index === ul_len){
+    function changeCarousel() {
+        if (current_index >= ul_len) {
             current_index = 0;
         }
 
-        for(var i = 0; i < ul_len; i++){
+        for (var i = 0; i < ul_len; i++) {
             dot_li[i].style.backgroundColor = "#fff";
         }
 
-        carousels.style.backgroundImage = "url(../img/" + current_index + ".jpg)";
+        carousels.style.backgroundImage = "url(C:/Users/Wu/Pictures/img/" + current_index + ".jpg)";
         dot_li[current_index].style.backgroundColor = "red";
     }
 
     carousels.addEventListener("mouseover", stopInterval, false);
-    function stopInterval(){
+
+    function stopInterval() {
         clearInterval(timer);
         left_btn.style.display = "block";
         right_btn.style.display = "block";
     }
 
     carousels.addEventListener("mouseout", startInterval, false);
-    function startInterval(){
+
+    function startInterval() {
         timer = setInterval(loop, 2000);
         left_btn.style.display = "none";
         right_btn.style.display = "none";
     }
 
     left_btn.addEventListener("click", carouselLeft, false);
-    function carouselLeft(){
+
+    function carouselLeft() {
         current_index--;
         changeCarousel();
     }
 
-    left_btn.addEventListener("click", carouselRight, false);
-    function carouselLeft(){
-        current_index--;
+    right_btn.addEventListener("click", carouselRight, false);
+
+    function carouselRight() {
+        current_index++;
         changeCarousel();
     }
 
-}
+    for (var j = 0; j < ul_len; j++) {
+        (function() {
+            var k = j;
+            dot_li[k].onmouseover = function() {
+                current_index = k;
+                console.log(k);
+                changeCarousel();
+            }
+        })();
+        // dot_li[i].onmouseover = function() {
+        //     current_index = parseInt(this.innerHTML) - 1;
+        //     changeCarousel();
+        // };
+    }
+
+};
